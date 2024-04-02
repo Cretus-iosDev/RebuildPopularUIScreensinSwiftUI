3825fa187466640767bba639c5e9693a60c44267
Cretus-iosDev <cretus.iosdev@gmail.com>
2024-04-02 10:15:30 +0530

    Initial Commit

---
+++ RebuildPopularUIScreensinSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RebuildPopularUIScreensinSwiftUI
+//
+//  Created by Dr. Shrikant Maraskolhe on 02/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RebuildPopularUIScreensinSwiftUI/RebuildPopularUIScreensinSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebuildPopularUIScreensinSwiftUIApp.swift
+//  RebuildPopularUIScreensinSwiftUI
+//
+//  Created by Dr. Shrikant Maraskolhe on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RebuildPopularUIScreensinSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
